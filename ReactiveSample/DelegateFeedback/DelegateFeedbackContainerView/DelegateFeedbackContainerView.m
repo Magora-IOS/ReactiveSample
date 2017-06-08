1bfd6ed7e6e634353897e72b24a3b1b5fe35af23
Michael Kapelko <kapelko@magora-systems.com>
2017-06-08 14:09:20 +0700

    Display result of selections

--- ReactiveSample/DelegateFeedback/DelegateFeedbackContainerView/DelegateFeedbackContainerView.m
+++ ReactiveSample/DelegateFeedback/DelegateFeedbackContainerView/DelegateFeedbackContainerView.m
@@ -7,7 +7,9 @@
 #import "UIView+Embed.h"
 #import "UIView+NibFile.h"
 
-@interface DelegateFeedbackContainerView()
+@interface DelegateFeedbackContainerView() <DelegateFeedbackViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
 
 @property (nonatomic, strong) IBOutlet UIView *feedbackOneContainerView;
 @property (nonatomic, strong) DelegateFeedbackView *feedbackOne;
@@ -35,6 +37,7 @@
     // One.
     self.feedbackOne = [DelegateFeedbackView loadFromNib];
     [self.feedbackOneContainerView embedView:self.feedbackOne];
+    self.feedbackOne.delegate = self;
     self.feedbackOne.title = LS(@"Feedback.One.Title");
     self.feedbackOne.acceptTitle = LS(@"Feedback.One.Yes");
     self.feedbackOne.declineTitle = LS(@"Feedback.One.No");
@@ -42,6 +45,7 @@
     // Two.
     self.feedbackTwo = [DelegateFeedbackView loadFromNib];
     [self.feedbackTwoContainerView embedView:self.feedbackTwo];
+    self.feedbackTwo.delegate = self;
     self.feedbackTwo.title = LS(@"Feedback.Two.Title");
     self.feedbackTwo.acceptTitle = LS(@"Feedback.Two.Yes");
     self.feedbackTwo.declineTitle = LS(@"Feedback.Two.No");
@@ -49,9 +53,29 @@
     // Three.
     self.feedbackThree = [DelegateFeedbackView loadFromNib];
     [self.feedbackThreeContainerView embedView:self.feedbackThree];
+    self.feedbackThree.delegate = self;
     self.feedbackThree.title = LS(@"Feedback.Three.Title");
     self.feedbackThree.acceptTitle = LS(@"Feedback.Three.Yes");
     self.feedbackThree.declineTitle = LS(@"Feedback.Three.No");
+    
+    // Result.
+    [self updateResult];
+}
+
+- (void)delegateFeedbackView:(DelegateFeedbackView *)feedback
+    status:(DelegateFeedbackViewStatus)status {
+
+    [self updateResult];
+}
+
+- (void)updateResult {
+    NSString *format = LS(@"Delegate.Result");
+    self.titleLabel.text =
+        [NSString
+            stringWithFormat:format,
+            @(self.feedbackOne.status),
+            @(self.feedbackTwo.status),
+            @(self.feedbackThree.status)];
 }
 
 @end
