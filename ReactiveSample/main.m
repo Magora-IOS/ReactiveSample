841e1a4a5c4fa23a1ae4528f2e68f415ca2db0b5
Michael Kapelko <kapelko@magora-systems.com>
2017-06-08 11:14:58 +0700

    Initial Commit

---
+++ ReactiveSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveSample
+//
+//  Created by Michael Kapelko on 08/06/17.
+//  Copyright © 2017 Magora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
